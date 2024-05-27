142ea402ff79f5a4e873cc593d71351a57f06b80
B3nnnji <bartekbanaszak5@gmail.com>
2024-05-27 19:50:41 +0200

    local multiplayer added

--- Program.cs
+++ Program.cs
@@ -1,94 +1,123 @@
 ﻿using System;
-using System.IO;
 
-namespace NumbersGuessingGame;
-class Program
+namespace NumbersGuessingGame
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("!!!WYBIERZ POZIOM TRUDNOŚCI!!!");
-        Console.WriteLine();
-        Console.WriteLine("1. Łatwy (1 - 100)");
-        Console.WriteLine();
-        Console.WriteLine("2. Normalny (1 - 10,000)");
-        Console.WriteLine();
-        Console.WriteLine("3. Trudny (1 - 1,000,000)");
-        Console.WriteLine();
-
-        int difficulty = Convert.ToInt32(Console.ReadLine());
-        int minRange = 1;
-        int maxRange = 100;
-
-        switch (difficulty)
+        static void Main(string[] args)
         {
-            case 1:
-                maxRange = 100;
-                Console.WriteLine();
-                Console.WriteLine("WYBRANY POZIOM TRUDNOŚCI: ŁATWY");
-                Console.WriteLine();
-                break;
-            case 2:
-                maxRange = 10000;
-                Console.WriteLine();
-                Console.WriteLine("WYBRANY POZIOM TRUDNOŚCI: NORMALNY");
-                Console.WriteLine();
-                break;
-            case 3:
-                maxRange = 1000000;
-                Console.WriteLine();
-                Console.WriteLine("WYBRANY POZIOM TRUDNOŚCI: TRUDNY");
-                Console.WriteLine();
-                break;
-            default:
-                Console.WriteLine("Niepoprawny wybór poziomu trudności. Ustawiam poziom łatwy.");
-                maxRange = 100;
-                break;
-        }
+            Console.WriteLine("!!!WYBIERZ POZIOM TRUDNOŚCI!!!");
+            Console.WriteLine();
+            Console.WriteLine("1. Łatwy (1 - 100)");
+            Console.WriteLine();
+            Console.WriteLine("2. Normalny (1 - 10,000)");
+            Console.WriteLine();
+            Console.WriteLine("3. Trudny (1 - 1,000,000)");
+            Console.WriteLine();
 
-        Console.WriteLine("!!!WYBIERZ TRYB GRY!!!");
-        Console.WriteLine();
-        Console.WriteLine("1. Ty zgadujesz.");
-        Console.WriteLine();
-        Console.WriteLine("2. Program zgaduje. Pamiętaj aby jako nazwę gracza wpisać słowo \"program\",");
-        Console.WriteLine("w przeciwnym razie zapis danych będzie nieprawidłowy!!!");
-        Console.WriteLine();
-        Console.WriteLine("3. Zgadywanie odbywa się na zmianę.");
-        Console.WriteLine();
+            int difficulty = Convert.ToInt32(Console.ReadLine());
+            int minRange = 1;
+            int maxRange;
 
-        int input = Convert.ToInt32(Console.ReadLine());
-        switch (input)
-        {
-            case 1:
-                Console.WriteLine();
-                Console.WriteLine("WYBRANY TRYB GRY: 1");
-                Console.WriteLine();
-                Player player1 = new Player();
-                player1.CreatePlayer();
-                Game game = new Game(player1, minRange, maxRange);
-                game.StartGame();
-                break;
-            case 2:
-                Console.WriteLine();
-                Console.WriteLine("WYBRANY TRYB GRY: 2");
-                Console.WriteLine();
-                Player player2 = new Player();
-                player2.CreatePlayer();
-                GameVsBot bot = new GameVsBot(player2);
-                bot.StartGameVsBot();
-                break;
-            case 3:
-                Console.WriteLine();
-                Console.WriteLine("WYBRANY TRYB GRY: 3");
-                Console.WriteLine();
-                Player player3 = new Player();
-                player3.CreatePlayer();
-                MixedGame mixedGame = new MixedGame(player3, minRange, maxRange);
-                mixedGame.StartMixedGame();
-                break;
-            default:
-                Console.WriteLine("Niepoprawny wybór trybu gry. Ustawiam tryb numer jeden!");
-                break;
+            switch (difficulty)
+            {
+                case 1:
+                    maxRange = 100;
+                    Console.WriteLine();
+                    Console.WriteLine("WYBRANY POZIOM TRUDNOŚCI: ŁATWY");
+                    Console.WriteLine();
+                    break;
+                case 2:
+                    maxRange = 10000;
+                    Console.WriteLine();
+                    Console.WriteLine("WYBRANY POZIOM TRUDNOŚCI: NORMALNY");
+                    Console.WriteLine();
+                    break;
+                case 3:
+                    maxRange = 1000000;
+                    Console.WriteLine();
+                    Console.WriteLine("WYBRANY POZIOM TRUDNOŚCI: TRUDNY");
+                    Console.WriteLine();
+                    break;
+                default:
+                    Console.WriteLine("Niepoprawny wybór poziomu trudności. Ustawiam poziom łatwy.");
+                    maxRange = 100;
+                    break;
+            }
+
+            Console.WriteLine("!!!WYBIERZ TRYB GRY!!!");
+            Console.WriteLine();
+            Console.WriteLine("1. Ty zgadujesz.");
+            Console.WriteLine();
+            Console.WriteLine("2. Program zgaduje. Pamiętaj aby jako nazwę gracza wpisać słowo \"program\",");
+            Console.WriteLine("w przeciwnym razie zapis danych będzie nieprawidłowy!!!");
+            Console.WriteLine();
+            Console.WriteLine("3. Zgadywanie odbywa się na zmianę.");
+            Console.WriteLine();
+            Console.WriteLine("4. Multiplayer.");
+            Console.WriteLine();
+
+            int input = Convert.ToInt32(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine();
+                    Console.WriteLine("WYBRANY TRYB GRY: 1");
+                    Console.WriteLine();
+                    Player player1 = new Player();
+                    player1.CreatePlayer();
+                    Game game = new Game(player1, minRange, maxRange);
+                    game.StartGame();
+                    break;
+                case 2:
+                    Console.WriteLine();
+                    Console.WriteLine("WYBRANY TRYB GRY: 2");
+                    Console.WriteLine();
+                    Player player2 = new Player();
+                    player2.CreatePlayer();
+                    GameVsBot bot = new GameVsBot(player2);
+                    bot.StartGameVsBot();
+                    break;
+                case 3:
+                    Console.WriteLine();
+                    Console.WriteLine("WYBRANY TRYB GRY: 3");
+                    Console.WriteLine();
+                    Player player3 = new Player();
+                    player3.CreatePlayer();
+                    MixedGame mixedGame = new MixedGame(player3, minRange, maxRange);
+                    mixedGame.StartMixedGame();
+                    break;
+                case 4:
+                    Console.WriteLine();
+                    Console.WriteLine("Wybrany tryb gry: Multiplayer");
+                    Console.WriteLine();
+                    Console.WriteLine("Podaj ilość graczy (2-4).");
+                    int playersNumber = Convert.ToInt32(Console.ReadLine());
+                    if (playersNumber < 2 || playersNumber > 4)
+                    {
+                        Console.WriteLine("Niepoprawna liczba graczy. Ustawiam 2 graczy.");
+                        playersNumber = 2;
+                    }
+
+                    Player[] players = new Player[playersNumber];
+                    for (int i = 0; i < playersNumber; i++)
+                    {
+                        players[i] = new Player();
+                        Console.WriteLine($"Podaj nazwę gracza {i + 1}:");
+                        players[i].Nickname = Console.ReadLine();
+                    }
+
+                    Multiplayer multiplayerGame = new Multiplayer(players, minRange, maxRange);
+                    multiplayerGame.StartMultiplayerGame();
+                    break;
+                default:
+                    Console.WriteLine("Niepoprawny wybór trybu gry. Ustawiam tryb numer jeden!");
+                    Player player4 = new Player();
+                    player4.CreatePlayer();
+                    Game game1 = new Game(player4, minRange, maxRange);
+                    game1.StartGame();
+                    break;
+            }
         }
     }
 }
-
